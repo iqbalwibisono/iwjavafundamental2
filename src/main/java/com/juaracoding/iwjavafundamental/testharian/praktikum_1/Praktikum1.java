ac3669b86f074c49b9f19191fd2ac252b56679ce
Iqbal Wibisono <iqbalwibisono28@gmail.com>
2023-01-14 19:39:17 +0700

    Revisi Commit

---
+++ src/main/java/com/juaracoding/iwjavafundamental/testharian/praktikum_1/Praktikum1.java
@@ -0,0 +1,13 @@
+package com.juaracoding.iwjavafundamental.testharian.praktikum_1;
+
+/*
+IntelliJ IDEA 2022.3.1 (Community Edition)
+Build #IC-223.8214.52, built on December 20, 2022
+@Author Iqbal Wibisono  a.k.a Iqbal Wibisono
+Java Developer
+Created on 14/01/2023 19:38
+@Last Modified 14/01/2023 19:38
+Version 1.1
+*/
+public class Praktikum1 {
+}
