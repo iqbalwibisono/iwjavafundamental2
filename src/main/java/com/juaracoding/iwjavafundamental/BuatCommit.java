0a4771b60484115789b34e07a3667f1eac6315f0
Iqbal Wibisono <iqbalwibisono28@gmail.com>
2023-01-14 20:01:17 +0700

    test commit

---
+++ src/main/java/com/juaracoding/iwjavafundamental/BuatCommit.java
@@ -0,0 +1,19 @@
+package com.juaracoding.iwjavafundamental;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+/*
+IntelliJ IDEA 2022.3.1 (Community Edition)
+Build #IC-223.8214.52, built on December 20, 2022
+@Author Iqbal Wibisono  a.k.a Iqbal Wibisono
+Java Developer
+Created on 14/01/2023 20:00
+@Last Modified 14/01/2023 20:00
+Version 1.1
+*/
+public class BuatCommit {
+
+    public static void main(String[] args) {
+
+    }
+}
