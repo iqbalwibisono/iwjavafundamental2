00faf762b4a07325770fc050a1bc67f060ce1337
Iqbal Wibisono <iqbalwibisono28@gmail.com>
2023-02-01 18:57:33 +0700

    .....

---
+++ src/main/java/com/juaracoding/iwjavafundamental/testharian/JVMday10.java
@@ -0,0 +1,63 @@
+package com.juaracoding.iwjavafundamental.testharian;
+
+/*
+IntelliJ IDEA 2022.3.1 (Community Edition)
+Build #IC-223.8214.52, built on December 20, 2022
+@Author Iqbal Wibisono  a.k.a Iqbal Wibisono
+Java Developer
+Created on 16/01/2023 19:19
+@Last Modified 16/01/2023 19:19
+Version 1.1
+*/
+
+//void=signature
+//method pake camelCase
+//nama methodnya sampleMethod
+//nilai return harus sesuai
+//modifier = hak akses
+//modifier public bisa dipanggil dimanapun
+//manggilnya pake dot
+//import buat panggil dari package lain
+//public class JVMday10 {
+//
+//    private int intX;
+//
+//    public int intX;
+//
+//    protected int intX;
+//
+//
+//
+//    public static void main(String[] args) {
+//        int intAngka;
+//
+//    }
+//
+//    public boolean sampleMethod(int intX){
+//        System.out.println("Hello");
+//        int intX=4;
+//        boolean isGood=true;
+//
+//        if(intX ==4){
+//
+//            isGood = false;
+//
+//
+//        }
+//
+//        return isGood;
+//
+//    }
+//
+//    protected void sampleMethodTwo(){
+//
+//    }
+//
+//    private void sampleMethodThree(){
+//
+//    }
+//
+//    void sampleMethodFour(){
+//
+//    }
+//}
